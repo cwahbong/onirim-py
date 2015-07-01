85b40afe06ede9e5299177602587541909913adc
Ahbong Chang <cw.ahbong@gmail.com>
2015-07-02 00:06:08 +0800

    Fix console demo

---
+++ console_demo.py
@@ -0,0 +1,6 @@
+import onirim.agent
+import onirim.core
+import onirim.data
+
+
+onirim.core.run(onirim.agent.console(), onirim.data.starting_content())
--- test.py
+++
@@ -1,8 +0,0 @@
-import onirim.card
-import onirim.data
-import onirim.core
-
-#for card in onirim.data.basic_cards():
-#    print(card)
-
-onirim.core.Onirim(onirim.data.basic_cards()).run()
